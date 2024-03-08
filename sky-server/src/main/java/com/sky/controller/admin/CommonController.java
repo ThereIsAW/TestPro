f469f4440a1dd549d45a70c049e974bfdb4fae11
Administrator <001@123.com>
2024-03-08 11:40:31 +0800

    项目初始代码1.0(写完华为OBS代码，未试运行)

---
+++ sky-server/src/main/java/com/sky/controller/admin/CommonController.java
@@ -0,0 +1,52 @@
+package com.sky.controller.admin;
+
+import com.sky.result.Result;
+import com.sky.utils.HuaWeiObsUtil;
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/admin/common")
+@Api(tags = "通用业务")
+@Slf4j
+public class CommonController {
+
+    @Autowired
+    private HuaWeiObsUtil huaWeiObsUtil;
+
+    public Result<String> upload(MultipartFile file){
+
+        String filesPath;
+
+        try {
+
+            //获取文件后缀名
+            String originalFilename = file.getOriginalFilename();
+            String extension = originalFilename.substring(originalFilename.lastIndexOf("."));
+            //生成UUID作为文件名
+            String objectName = UUID.randomUUID().toString() + extension;
+
+            File upFile = new File("com/sky/mealPicture" + objectName);
+            file.transferTo(upFile);
+
+            filesPath = huaWeiObsUtil.uploadFiles(upFile);
+
+            upFile.delete();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return Result.success(filesPath);
+
+    }
+
+}
