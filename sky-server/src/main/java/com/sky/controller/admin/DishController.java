ecd05b9d9c4ae546aa47477dd12354fa063bd770
Administrator <001@123.com>
2024-03-09 16:48:16 +0800

    项目初始代码1.0(写到菜品模块)

---
+++ sky-server/src/main/java/com/sky/controller/admin/DishController.java
@@ -0,0 +1,16 @@
+package com.sky.controller.admin;
+
+import io.swagger.annotations.Api;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/dish")
+@Slf4j
+@Api(tags = "菜品管理模块")
+public class DishController {
+
+
+
+}
