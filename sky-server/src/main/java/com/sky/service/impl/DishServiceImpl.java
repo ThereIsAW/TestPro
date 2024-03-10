240f1917e1408bb0cf010c2a294917b7b7c06db7
Administrator <001@123.com>
2024-03-10 10:56:09 +0800

    项目初始代码1.0(写到菜品模块)

--- sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/DishServiceImpl.java
@@ -2,11 +2,16 @@ package com.sky.service.impl;
 
 import com.sky.dto.DishDTO;
 import com.sky.entity.Dish;
+import com.sky.entity.DishFlavor;
+import com.sky.mapper.DishFlavorMapper;
 import com.sky.mapper.DishMapper;
 import com.sky.service.DishService;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
 
 @Service
 public class DishServiceImpl implements DishService {
@@ -14,11 +19,15 @@ public class DishServiceImpl implements DishService {
     @Autowired
     DishMapper dishMapper;
 
+    @Autowired
+    DishFlavorMapper dishFlavorMapper;
+
     /**
      * 增加菜品
      * @param dishDTO
      */
     @Override
+    @Transactional
     public void save(DishDTO dishDTO) {
 
         Dish dish = new Dish();
@@ -28,7 +37,20 @@ public class DishServiceImpl implements DishService {
         //保存新增的菜品
         dishMapper.save(dish);
 
-        //保存新增的菜品对应口味
+        Long dishId = dish.getId();
+
+        List<DishFlavor> dishFlavorList = dishDTO.getFlavors();
+
+        if(dishFlavorList != null && dishFlavorList.size() > 0){
+
+            dishFlavorList.forEach(dishFlavor -> {
+                dishFlavor.setId(dishId);
+            });
+
+            //保存新增的菜品对应口味
+            dishFlavorMapper.saveFlavor(dishFlavorList);
+
+        }
 
     }
 }
