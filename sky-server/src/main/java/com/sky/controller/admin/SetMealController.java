7576359c60eafd7b463142161f88a6453f5b22c8
Administrator <001@123.com>
2024-03-15 10:59:14 +0800

    项目初始代码1.0(完成菜品模块)

---
+++ sky-server/src/main/java/com/sky/controller/admin/SetMealController.java
@@ -0,0 +1,117 @@
+package com.sky.controller.admin;
+
+import com.sky.dto.SetmealDTO;
+import com.sky.dto.SetmealPageQueryDTO;
+import com.sky.result.PageResult;
+import com.sky.result.Result;
+import com.sky.service.SetMealService;
+import com.sky.vo.SetmealVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/admin/setmeal")
+@Api(tags = "套餐管理")
+@Slf4j
+public class SetMealController {
+
+    @Autowired
+    private SetMealService setMealService;
+
+    /**
+     * 套餐分页查询
+     * @param setmealPageQueryDTO
+     * @return
+     */
+    @GetMapping("/page")
+    @ApiOperation("套餐分页查询")
+    public Result<PageResult> pageQuery(SetmealPageQueryDTO setmealPageQueryDTO){
+
+        PageResult pageResult = setMealService.pageQuery(setmealPageQueryDTO);
+
+        return Result.success(pageResult);
+    }
+
+    /**
+     * 根据id修改套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PutMapping
+    @ApiOperation("根据id修改套餐")
+    public Result updateMeal(@RequestBody SetmealDTO setmealDTO){
+
+        setMealService.update(setmealDTO);
+
+        return Result.success();
+    }
+
+    /**
+     * 根据id查询套餐
+     * @param id
+     * @return
+     */
+    @GetMapping("/{id}")
+    @ApiOperation("根据id查询套餐")
+    public Result<SetmealVO> queryById(@PathVariable Long id){
+
+        SetmealVO setmealVO = setMealService.queryById(id);
+
+        return Result.success(setmealVO);
+
+    }
+
+    /**
+     * 新增套餐
+     * @param setmealDTO
+     * @return
+     */
+    @PostMapping
+    @ApiOperation("新增套餐")
+    public Result save(@RequestBody SetmealDTO setmealDTO){
+
+        setMealService.save(setmealDTO);
+
+        return Result.success();
+
+    }
+
+    /**
+     * 套餐起售、停售
+     * @param status
+     * @param id
+     * @return
+     */
+    @PostMapping("/status/{status}")
+    @ApiOperation("套餐起售、停售")
+    public Result changeStatus(@PathVariable Integer status,Long id){
+
+        setMealService.changeStatus(status,id);
+
+        return Result.success();
+
+    }
+
+    /**
+     * 批量删除套餐
+     * @param ids
+     * @return
+     */
+    @DeleteMapping
+    @ApiOperation("批量删除套餐")
+    public Result batchDelete(@RequestParam List<Long> ids){
+
+        setMealService.batchDelete(ids);
+
+        return Result.success();
+
+    }
+
+
+
+}
---
+++ sky-server/src/main/java/com/sky/service/impl/SetMealServiceImpl.java
@@ -0,0 +1,11 @@
+package com.sky.service.impl;
+
+import com.sky.service.SetMealService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SetMealServiceImpl implements SetMealService {
+
+
+
+}
